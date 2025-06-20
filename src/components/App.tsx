5a6915b086f3fa50c24626ae815f57fa7eac9419
Aram <aram.anto@gmail.com>
2025-06-20 11:34:39 +0200

    deleted unused import

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,3 @@
-import styles from "../styles/App.module.css";
 import LlistaTemporades from "./LlistaTemporades";
 import Temporada from "./Temporada";
 import { BrowserRouter, Routes, Route } from "react-router";
