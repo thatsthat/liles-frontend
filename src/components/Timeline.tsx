4e32fea7448d1325348842ee770f50edbd780711
Aram <aram.anto@gmail.com>
2026-08-13 15:14:25 +0200

    Added state isLoading when calling api in useRef. Added skeleton elements during api data fetching

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -6,7 +6,7 @@ import { loggedIn } from "../utils/userInfo";
 import { type TemporadaT } from "./types";
 import Skeleton from "react-loading-skeleton";
 
-const SKELETON_COUNT = 6;
+const SKELETON_COUNT = 4;
 
 function Timeline() {
   const id = useParams().temporadaId;
@@ -35,33 +35,31 @@ function Timeline() {
           <div className={styles.timeline}>
             {loading
               ? Array.from({ length: SKELETON_COUNT }).map((_, i) => (
-                  <div
-                    key={i}
-                    className={`${styles.container} ${
-                      i % 2 == 0 ? styles.left : styles.right
+                <div
+                  key={i}
+                  className={`${styles.container} ${i % 2 == 0 ? styles.left : styles.right
                     }`}
-                  >
-                    <div className={styles.content}>
-                      <h2>
-                        <Skeleton width={80} />
-                      </h2>
-                    </div>
+                >
+                  <div className={styles.content}>
+                    <h2>
+                      <Skeleton width={80} />
+                    </h2>
                   </div>
-                ))
+                </div>
+              ))
               : Array.isArray(data) &&
-                data.map((temporada: TemporadaT, i: number) => (
-                  <div
-                    className={`${styles.container} ${
-                      i % 2 == 0 ? styles.left : styles.right
+              data.map((temporada: TemporadaT, i: number) => (
+                <div
+                  className={`${styles.container} ${i % 2 == 0 ? styles.left : styles.right
                     }`}
-                  >
-                    <Link to={"/temporada/" + temporada.id}>
-                      <div className={styles.content}>
-                        <h2>{temporada.year}</h2>
-                      </div>
-                    </Link>
-                  </div>
-                ))}
+                >
+                  <Link to={"/temporada/" + temporada.id}>
+                    <div className={styles.content}>
+                      <h2>{temporada.year}</h2>
+                    </div>
+                  </Link>
+                </div>
+              ))}
           </div>
         </div>
       </div>
